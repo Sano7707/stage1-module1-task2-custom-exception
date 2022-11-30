e6761dd815d5da3edced718a1347369cdd4e64dd
Sano <sanasar.hambardzumyan@gmail.com>
2022-11-30 23:48:49 +0400

    Changes

--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -1,11 +1,22 @@
 package com.epam.mjc;
 
 
+import java.util.Arrays;
+
 public class StudentManager {
 
   private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
-  public Student find(long studentID) {
+  public Student find(long studentID)throws StudentNotFoundException {
+    for (int i = 0; i < IDs.length; i++) {
+      if(Arrays.binarySearch(IDs,studentID) >= 0){
+
+      }
+      else {
+        throw new StudentNotFoundException("Could not find student with ID " + studentID);
+      }
+
+    }
     return Student.getValueOf(studentID);
   }
 
